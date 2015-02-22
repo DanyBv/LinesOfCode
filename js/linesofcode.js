85b7c4f4cf95999146536e8309f07fddfea563ad
DanyBv <dany_gagiu@yahoo.com>
2015-02-23 00:43:47 +0200

    Added buttons

---
+++ js/linesofcode.js
@@ -0,0 +1,32 @@
+$(":button").each(function (i) { $(this).attr('tabindex','-1'); });
+
+function stopRKey(evt) { 
+  var evt = (evt) ? evt : ((event) ? event : null); 
+  var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null); 
+  if ((evt.keyCode == 13) && (node.type=="button"))  {return false;} 
+} 
+
+document.onkeypress = stopRKey;
+
+var app = angular.module("LinesOfCodeApp", []);
+app.controller("LinesOfCodeCtrl", function($scope) {
+    $scope.LoC=0;
+
+    $scope.addLoC = function() {
+        $scope.LoC++;
+    };
+    
+    $scope.saveAll = function() {   
+       createCookie("loc", $scope.LoC, Infinity);
+    };
+    
+    $scope.resetAll = function() {   
+        eraseCookie("loc");
+        location.reload();
+    };
+    
+    $scope.init = function() {
+        if(readCookie("loc"))
+            $scope.LoC=readCookie("loc");
+    };
+});

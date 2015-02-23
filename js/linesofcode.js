9fb655ea75a3d089ffc4a067815ceffbab7909de
DanyBv <dany_gagiu@yahoo.com>
2015-02-23 23:39:50 +0200

    Added money and button to sell

--- js/linesofcode.js
+++ js/linesofcode.js
@@ -11,17 +11,25 @@ document.onkeypress = stopRKey;
 var app = angular.module("LinesOfCodeApp", []);
 app.controller("LinesOfCodeCtrl", function($scope) {
     $scope.LoC=0;
-
+    $scope.money=0.0;
+    
     $scope.addLoC = function() {
         $scope.LoC++;
     };
     
     $scope.saveAll = function() {   
        createCookie("loc", $scope.LoC, Infinity);
+       createCookie("money", $scope.money, Infinity);
     };
+    
+    $scope.sellAll = function() {
+        $scope.money+=0.5*$scope.LoC;
+        $scope.LoC=0;
+    }
     
     $scope.resetAll = function() {   
         eraseCookie("loc");
+        eraseCookie("money");
         location.reload();
     };
     
@@ -29,5 +37,8 @@ app.controller("LinesOfCodeCtrl", function($scope) {
         var loc=parseInt(readCookie("loc"));
         if(!isNaN(loc))
             $scope.LoC=loc;
+        var money=parseInt(readCookie("money"));
+        if(!isNaN(money))
+            $scope.money=money;
     };
 });

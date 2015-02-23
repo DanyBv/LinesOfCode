d0e3baf4539dc1b00769e16566e691a5d744d72f
DanyBv <dany_gagiu@yahoo.com>
2015-02-24 00:47:54 +0200

    Update

--- js/linesofcode.js
+++ js/linesofcode.js
@@ -1,4 +1,7 @@
 $(":button").each(function (i) { $(this).attr('tabindex','-1'); });
+$('[data-toggle="tooltip"]').tooltip({
+    'placement': 'top'
+});
 
 function stopRKey(evt) { 
   var evt = (evt) ? evt : ((event) ? event : null); 
@@ -9,9 +12,12 @@ function stopRKey(evt) {
 document.onkeypress = stopRKey;
 
 var app = angular.module("LinesOfCodeApp", []);
-app.controller("LinesOfCodeCtrl", function($scope) {
+app.controller("LinesOfCodeCtrl", function($scope, $timeout) {
     $scope.LoC=0;
     $scope.money=0.0;
+    $scope.sk=0;
+    $scope.skps=0;
+    $scope.skp=5;
     
     $scope.addLoC = function() {
         $scope.LoC++;
@@ -22,19 +28,35 @@ app.controller("LinesOfCodeCtrl", function($scope) {
        createCookie("money", $scope.money, Infinity);
     };
     
+    $scope.buyUnit = function($id) {
+        if($id==1)//Skilled kid
+        {
+            if($scope.money>=$scope.skp)
+            {
+                $scope.sk++;
+                $scope.skps+=0.25;
+                $scope.money-=$scope.skp;
+                $scope.skp*=1.01;
+            }
+        }
+    };
+    
     $scope.sellAll = function() {
-        $scope.money+=0.5*$scope.LoC;
+        $scope.money+=0.05*$scope.LoC;
         $scope.LoC=0;
     }
     
     $scope.resetAll = function() {   
         eraseCookie("loc");
         eraseCookie("money");
+        eraseCookie("sk");
+        eraseCookie("skp");
+        eraseCookie("sks");
         location.reload();
     };
     
     $scope.init = function() {
-        var loc=parseInt(readCookie("loc"));
+        var loc=parseFloat(readCookie("loc"));
         if(!isNaN(loc))
             $scope.LoC=loc;
         var money=parseFloat(readCookie("money"));
@@ -42,5 +64,13 @@ app.controller("LinesOfCodeCtrl", function($scope) {
             $scope.money=money;
     };
     
-    $scope.saveTimer = setInterval($scope.saveAll, 60000);
+    $scope.doThings = function() {
+      
+      $scope.LoC += $scope.skps;
+      $scope.LoC = Math.round($scope.LoC * 100) / 100;
+      
+    }
+    
+    $timeout( function(){ $scope.saveAll(); }, 60000);
+    $timeout( function(){ $scope.doThings(); }, 1000);
 });
